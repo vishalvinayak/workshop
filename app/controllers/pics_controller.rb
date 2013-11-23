e206ef6df2012b08413943357a50965d255d272b
Vishal <vishal@Vishal-VirtualBox.(none)>
2013-11-23 15:43:37 -0500

    Afer Form

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,26 @@
 class PicsController < ApplicationController
 
+	def create
+		# Pic.create(:lesson => )
+
+		@pic = Pic.create( pic_params)
+
+		redirect_to pics_path
+	end
+
+	def new
+		@pic = Pic.new
+	end
+
 	def index
 		@pics = Pic.all
 
 	end
+
+	private
+
+	def pic_params
+		params.require(:pic).permit(:lesson, :emotion, :learned)
+	end
+
 end
